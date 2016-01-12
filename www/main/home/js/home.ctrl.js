e3a671f2d570e3cac0583228875f87b3c31137af
kirti <siddharth>
2016-01-13 02:20:03 +0530

    worked on home page

--- www/main/home/js/home.ctrl.js
+++ www/main/home/js/home.ctrl.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
     angular.module('beer.home').controller('HomeCtrl', homeCtrl);
-    function homeCtrl($scope, uiGmapGoogleMapApi, ajaxRequest, $log, $ionicPopup)
+    function homeCtrl($scope, uiGmapGoogleMapApi, geoHelper, $log, $ionicPopup)
     {
 
 
@@ -10,35 +10,48 @@
 // This method accepts a Position object, which contains the
 // current GPS coordinates
 //
-      $scope.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
-        var onSuccess = function (position) {
-            console.log(position);
-                    var promise2 = ajaxRequest.send('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=true');
-            promise2.then(function (data) {
-              console.log(data.results[0].formatted_address);
-              $scope.location=data.results[0].formatted_address;
-            });
+        $scope.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
+        uiGmapGoogleMapApi.then(function (maps) {
+        });
+        $scope.marker = {
+            id: 0,
+            coords: {
+                latitude: 40.1451,
+                longitude: -99.6680
+            },
+            options: {draggable: true,
+                scrollwheel: true},
+            events: {
+                dragend: function (marker, eventName, args) {
+                    $log.log('marker dragend');
+                    var lat = marker.getPosition().lat();
+                    var lon = marker.getPosition().lng();
+                    $log.log(lat);
+                    $log.log(lon);
 
+                    $scope.marker.options = {
+                        draggable: true,
+                        labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                        labelAnchor: "100 0",
+                        labelClass: "marker-labels"
+                    };
+                }
+            }
         };
-  uiGmapGoogleMapApi.then(function (maps) {
-            });
-// onError Callback receives a PositionError object
-//
-        function onError(error) {
-          
-      
-            var promise = ajaxRequest.send('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBWkF7x23R6YZCUCu02fZnXVfNtBl4U2gs');
-            promise.then(function (data) {
-                 console.log(data);
-                var promise2 = ajaxRequest.send('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + data.location.lat + ',' + data.location.lng + '&sensor=true');
-                promise2.then(function (data) {
-                    console.log(data);
-                });
-               
-            });
-        }
-
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        $scope.randomMarkers = []
+        var promise = geoHelper.address();
+        promise.then(function (data) {
+            console.log(data);
+            var loc = data.results[0].geometry.location;
+            $scope.map = {center: {latitude: loc.lat, longitude: loc.lng}, zoom: 15};
+            $scope.marker.coords.latitude = loc.lat;//setting the user location on the map
+            $scope.marker.coords.longitude = loc.lng;//setting the user location on the map
+            $scope.randomMarkers = [{latitude: loc.lat + 0.1,ran:'main/image/icon.png', longitude: loc.lng + 0.1, title: 'm' + 1, id: 0}];
+        });
+        promise.catch(function (data) {
+            console.log('gps not avialable')
+        });
+        $scope.ran = 'main/image/icon.png';
 
         var myPopup = $ionicPopup.show({
             template: 'Please Enable Gps To Get Best From This App',
@@ -58,13 +71,12 @@
         });
         function saveButton(e) {
 
-
         }
         function cancelButton(e) {
 
         }
         myPopup.then(function (res) {
-            alert('Tapped!', res);
+
             myPopup.close();
         });
 
--- www/main/service/geolocation.js
+++ www/main/service/geolocation.js
@@ -2,12 +2,38 @@
     'use strict';
     angular.module('beer.service', [])
             .factory('geoHelper', geoHelper);
-    function geoHelper() {
+    function geoHelper(ajaxRequest, $q) {
         var geo = {};
-        geo.address = function (data) {
+        geo.defer = $q.defer();
+        geo.timeout=30000;
+        geo.success = function (position) {
+            
+            var promise2 = ajaxRequest.send('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&sensor=true');
+            promise2.then(function (data) {
+                geo.defer.resolve(data);
+            });
+        };
+        geo.error = function (error) {
+            var promise = ajaxRequest.send('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBWkF7x23R6YZCUCu02fZnXVfNtBl4U2gs');
+            promise.then(function (data) {
+                var promise2 = ajaxRequest.send('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + data.location.lat + ',' + data.location.lng + '&sensor=true');
+                promise2.then(function (data) {
+                    geo.defer.reject(data);
+                });
 
+            });
+        }
+        geo.address = function (timeout) {
+            if(timeout)
+                geo.timeout=timeout;
+           
+            navigator.geolocation.watchPosition(geo.success, geo.error,{ maximumAge: 3000, timeout: geo.timeout, enableHighAccuracy: true });//function for get position of user
+            return  geo.defer.promise;
         };
         return geo;
     }
     ;
 })();
+
+
+
